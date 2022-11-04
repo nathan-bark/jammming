a6c421249fbeb6a0cdd70e90ce137a7968a3dbe3
Nathan <nathanbark4@gmail.com>
2022-11-04 21:31:36 +0200

    pass down and render results to step 35

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Track.css';
+
+export class Track extends React.Component {
+  renderAction() {
+    return <button className="Track-action">{!this.props.isRemoval ? "+" : "-"}</button>;
+  }
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
+
+  
+}
